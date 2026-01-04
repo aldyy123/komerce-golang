983c0ee2a9aa0fb31d065513b1827198b22f394b
Aldyy123 <mohammad.aldyy@gmail.com>
2026-01-04 13:59:33 +0700

    add some sample case

--- main.go
+++ main.go
@@ -11,9 +11,18 @@ func main() {
 	fmt.Println("Vowels:", vowels)
 	fmt.Println("Consonants:", consonants)
 
+	vowels, consonants = testcase.VowelsAndConsonants("Next Case")
+	fmt.Println("Vowels:", vowels)
+	fmt.Println("Consonants:", consonants)
+
+	fmt.Println("--------------------------------")
+
 	psbb := testcase.PSBB(5, []int{1, 2, 4, 3, 3})
-	fmt.Println("PSBB:", psbb)
+	fmt.Println("PSBB 1:", psbb)
 
 	psbb = testcase.PSBB(8, []int{2, 3, 4, 4, 2, 1, 3, 1})
-	fmt.Println("PSBB:", psbb)
+	fmt.Println("PSBB 2:", psbb)
+
+	psbb = testcase.PSBB(5, []int{15})
+	fmt.Println("PSBB 3:", psbb)
 }
--- test_case/psbb.go
+++ test_case/psbb.go
@@ -6,26 +6,20 @@ import (
 )
 
 func PSBB(n int, arr []int) int {
+	buses := 0
 
 	if n != len(arr) {
-		fmt.Print("Input must be equal with count of family")
-		return -1
+		fmt.Println("Input must be equal with count of family")
+		return buses
 	}
 
 	sort.Sort(sort.Reverse(sort.IntSlice(arr)))
-
-	buses := 0
 	used := make([]bool, n)
 
 	for i := 0; i < len(arr); i++ {
-		if used[i] {
-			continue
-		}
-
 		buses++
 		used[i] = true
 		remaining := 4 - arr[i]
-
 		if remaining > 0 {
 			for j := i + 1; j < n; j++ {
 				if !used[j] && arr[j] <= remaining {
